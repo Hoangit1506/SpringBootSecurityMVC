86902201a550c70144475711850e4d2d2b2989c3
Hoangit1506 <hoangit1506@gmail.com>
2024-10-05 19:58:59 +0700

    Initial project

---
+++ src/main/java/com/springweb/SpringBootSecurityMVC/SpringBootSecurityMvcApplication.java
@@ -0,0 +1,13 @@
+package com.springweb.SpringBootSecurityMVC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSecurityMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSecurityMvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springweb/SpringBootSecurityMVC/SpringBootSecurityMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springweb.SpringBootSecurityMVC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSecurityMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
